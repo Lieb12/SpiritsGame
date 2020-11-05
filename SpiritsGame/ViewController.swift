d98e837ca80f2f46c615450faf47dbcfd8103d02
Lieb12 <lieberman03@gmail.com>
2020-11-05 08:22:37 -0500

    Initial Commit

---
+++ SpiritsGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpiritsGame
+//
+//  Created by  on 11/5/20.
+//  Copyright © 2020 Owen Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
