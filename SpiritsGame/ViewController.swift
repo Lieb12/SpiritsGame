6d4ae5e6241865f4c6873c2e33a0ec426f75cc4a
Lieb12 <lieberman03@gmail.com>
2020-11-06 09:15:49 -0500

    menu screen

--- SpiritsGame/ViewController.swift
+++ SpiritsGame/ViewController.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    let LadyRadical = UIFont(name: "/fonts/LadyRadical.ttf", size: 24)
+    
+    // TODO: Set save points
+    // let defaults = UserDefaults.standard
     
     @IBOutlet var titleTapRec: UITapGestureRecognizer!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tapLabel: UILabel!
+
     
-    let playerLvl: Int = 0
+    let level1Name = "1. Trade Secrets"
+    let level2Name = "2. Bleeding-heart"
+    let level3Name = "3. Encore"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        titleLabel.alpha = 1.0
+        tapLabel.alpha = 1.0
     }
 
     @IBAction func titleTapped(_ sender: Any) {
-        
+        UIView.animate(withDuration: 4, animations: {
+            self.tapLabel.alpha = 0.0;
+            self.titleLabel.alpha = 0.0
+            })
+        usleep(3000000)
+        performSegue(withIdentifier: "titleToMenu", sender: UITapGestureRecognizer.self)
     }
     
 }
